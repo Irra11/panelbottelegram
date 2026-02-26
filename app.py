100be537fabf8a6942459e6cdb65d4c95fad0edf
Irra11 <boraffcremix@gmail.com>
2026-02-27 00:46:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # 🔴 CHANGE THIS to the URL where your HTML is hosted
-FRONTEND_URL = "https://your-website-domain.com/"
+FRONTEND_URL = "https://panelbottelegram.vercel.app/"
 
 @app.route('/api/get-profile')
 def get_profile():
